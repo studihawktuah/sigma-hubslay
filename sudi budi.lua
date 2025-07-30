a0b73e4e96c43a55209387773bce317f2b3b8e24
studihawktuah <silversdamn@gmail.com>
2025-07-30 20:15:23 +0800

    Update sudi budi.lua

--- sudi budi.lua
+++ sudi budi.lua
@@ -4,7 +4,7 @@ local Window = Rayfield:CreateWindow({
 
    Name = "Secret Sigma Studi Hub",
 
-   Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
+   Icon = 112739248277964, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
 
    LoadingTitle = "welcome to studious hub",
 
