630200e893356377813ac45882df10d8f611e73d
c00lhackkscriptservice <iezafira11611@gmail.com>
2025-08-02 05:22:16 +0800

    Update sudi budi.lua

--- sudi budi.lua
+++ sudi budi.lua
@@ -109,5 +109,4 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/InfernusScripts/Fire-
 })
 local Button = Tab5:CreateButton({
    Name = "Dandys world script!",
-   Callback = function()
-https://pastebin.com/raw/Y7uh3UZf
+   Callback = function() loadstring(game:HttpGet("https://pastebin.com/raw/Y7uh3UZf"))();
