92b99b534cb3c14153658f96e8baab1b65f9b9ce
studihawktuah <silversdamn@gmail.com>
2025-08-02 22:42:50 +0800

    Update sudi budi.lua

--- sudi budi.lua
+++ sudi budi.lua
@@ -62,7 +62,7 @@ local Tab = Window:CreateTab("Main tab", 108606097801627) -- Title, Image
 local Tab2 = Window:CreateTab("grow a kid!", 4483362458) -- Title, Image
 local Tab3 = Window:CreateTab("Animations!", 6119363456) -- Title, Image
 local Tab4 = Window:CreateTab("pressure & more", 81417074044532) -- Title, Image
-local Tab5 = Window:CreateTab("dandys world!", 79143620854409) -- Title, Image
+local Tab5 = Window:CreateTab("dandys world!", 98074037951297) -- Title, Image
 local Tab6 = Window:CreateTab("forsaken/forsakenTD :P", 10753364424) -- Title, Image
 
 Rayfield:Notify({
