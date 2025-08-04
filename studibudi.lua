8d19c159cff67cba1fc2c1e27e62982ef7c97b3f
c00lhackkscriptservice <iezafira11611@gmail.com>
2025-08-05 04:32:47 +0800

    Update studibudi.lua

--- studibudi.lua
+++ studibudi.lua
@@ -170,7 +170,7 @@ loadstring(string.char(
 local Button = Tab6:CreateButton({
   Name = "forsaken TD script 2",
   Callback = function()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/L0stVeil/LocalScripts/refs/heads/main/LoaderFTD")()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/L0stVeil/LocalScripts/refs/heads/main/LoaderFTD"))()
    end,
 })
  
